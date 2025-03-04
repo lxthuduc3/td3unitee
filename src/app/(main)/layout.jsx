50734b9bbe4aec2f80af29323d2866e2b7e1c561
khiemnb153 <khiemnb153@gmail.com>
2025-03-04 16:10:40 +0700

    Try for safe area again again

--- src/app/(main)/layout.jsx
+++ src/app/(main)/layout.jsx
@@ -5,7 +5,7 @@ import NotificationProvider from '@/components/notification-provider'
 
 const MainLayout = ({ children }) => {
   return (
-    <main className='flex h-screen w-screen flex-col overflow-hidden'>
+    <main className='flex h-[calc(100%-env(safe-area-inset-top)-env(safe-area-inset-bottom))] w-screen flex-col overflow-hidden'>
       <Toaster position='top-center' />
       <AuthGuard>
         <NotificationProvider>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import ThemeProvider from '@/components/theme-provider'
 const RootLayout = ({ children }) => {
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_ID}>
-      <div className='font-plus-jakarta-sans antialiased'>
+      <div className='font-plus-jakarta-sans h-screen w-screen overflow-hidden pt-[env(safe-area-inset-top)] pr-[env(safe-area-inset-right)] pb-[env(safe-area-inset-bottom)] pl-[env(safe-area-inset-left)] antialiased'>
         <ThemeProvider>
           <AuthProvider>{children}</AuthProvider>
         </ThemeProvider>
