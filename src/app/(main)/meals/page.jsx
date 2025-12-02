52a7e9fa87386792a8f894fe223658ee00a9f046
theanhtran <ndu.trantheanh.180204@gmail.com>
2025-12-02 21:36:15 +0700

    Ver3.1.0: Add shopper role

--- src/app/(main)/meals/page.jsx
+++ src/app/(main)/meals/page.jsx
@@ -27,7 +27,7 @@ const MealsPage = () => {
             onClick={() => {
               setEditMode((prev) => !prev)
             }}
-            disabled={!(user?.role === 'executiveBoard' || user?.role === 'roomLeader')}
+            disabled={!(user?.role === 'executiveBoard' || user?.role === 'roomLeader' || user?.role === 'shopper')}
           >
             {editMode ? (
               <>
